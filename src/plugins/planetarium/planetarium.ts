1ad5a8fc72725f4a22a5174b0ecfeaf7db6f255a
A <49177898+bbgouzi123@users.noreply.github.com>
2026-09-17 21:09:17 +0800

    Clear repository contents

--- src/plugins/planetarium/planetarium.ts
+++
@@ -1,8 +0,0 @@
-import { t7e } from '@app/locales/keys';
-import { KeepTrackPlugin } from '../../engine/plugins/base-plugin';
-
-export class Planetarium extends KeepTrackPlugin {
-  init() {
-    console.error(t7e('plugins.proOnlyMsg'));
-  }
-}
--- src/plugins/telemetry/telemetry.ts
+++
@@ -1,8 +0,0 @@
-import { t7e } from '@app/locales/keys';
-import { KeepTrackPlugin } from '../../engine/plugins/base-plugin';
-
-export class Telemetry extends KeepTrackPlugin {
-  init() {
-    console.error(t7e('plugins.proOnlyMsg'));
-  }
-}
